63680f126b32406b3dc6c5300d446eea302d9a0e
chanex15 <167178682+chanex15@users.noreply.github.com>
2025-04-04 23:42:43 +0800

    Add files via upload

---
+++ PIT-3---Backend-main/backend/myapp/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework import viewsets  # Fixed typo
+
+@api_view(['GET', 'POST'])
+def post_list(request):
+    if request.method == 'GET':
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        print("Incoming Data:", request.data)
+        serializer = PostSerializer(data=request.data)  # Fixed typo
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+class PostViewSet(viewsets.ModelViewSet):  # Fixed typo
+    queryset = Post.objects.all().order_by('-created_at')
+    serializer_class = PostSerializer
